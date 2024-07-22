121b7e9ee25a73d5f98b323d42b8bf46d1d43f15
daedyu <134924951+daedyu@users.noreply.github.com>
2024-07-22 19:44:21 +0900

    feat: registerPage

---
+++ src/main/java/com/example/Jisi_Server/domain/user/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.example.Jisi_Server.domain.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserDTO {
+    private String phoneNumber;
+    private String username;
+    private String password;
+}
