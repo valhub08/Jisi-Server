e16620690346fee87a4d9647073062c28a7276b1
daedyu <134924951+daedyu@users.noreply.github.com>
2024-07-22 16:19:00 +0900

    Init Commit

---
+++ src/test/java/com/example/Jisi_Server/JisiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jisi_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JisiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
