3e9c71703e931f292b8766bf6b8a12fd0a6996ca
daedyu <134924951+daedyu@users.noreply.github.com>
2024-07-31 14:00:52 +0900

    feat: swagger-ui

--- src/main/java/com/example/Jisi_Server/global/config/SwaggerConfig.java
+++ src/main/java/com/example/Jisi_Server/global/config/SwaggerConfig.java
@@ -1,35 +1,17 @@
 package com.example.Jisi_Server.global.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SwaggerConfig {
-    @Bean
-    public OpenAPI openAPI() {
-        String jwt = "JWT";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
-        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
-                .name(jwt)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-        );
-        return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo())
-                .addSecurityItem(securityRequirement)
-                .components(components);
-    }
-    private Info apiInfo() {
-        return new Info()
-                .title("JISI-SERVER") // API의 제목
-                .description("jisi 메인 서버를 위한 ") // API에 대한 설명
-                .version("1.0.0"); // API의 버전
+@OpenAPIDefinition(info = @Info(title = "Jisi-API 문서", version = "v1"))
+public class SwaggerConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/4.15.5/");
     }
 }
